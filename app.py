941e04b40ca5e5e60e8d9887f715a70b93ffd44c
Robbe Van Herck <robbe@robbevanherck.be>
2020-06-21 18:12:12 +0000

    Basic deployable config

--- app.py
+++ app.py
@@ -3,42 +3,28 @@
 import mattermost
 import mattermost.ws
 
+from flask import request
 from flask import Flask
 app = Flask(__name__)
 
 import configparser
 import sys
 
+# Read in the config
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Setup Mattermost connection
+mm = mattermost.MMApi(config["mattermost"]["url"])
+mm.login(bearer=config["mattermost"]["token"])
+user = mm.get_user()
+
 def eprint(msg):
     print(msg, file=sys.stderr)
 
-mm = None
-user = None
-
-def main():
-    # Read in the config
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    
-    # Setup Mattermost connection
-    mm = mattermost.MMApi(config["mattermost"]["url"])
-    mm.login(bearer=config["mattermost"]["token"])
-    user = mm.get_user()
-
-    # Check for team id
-    if "team_id" not in config["mattermost"]:
-        available_teams = mm.get_teams()
-        eprint("No team_id set in config.ini, please select one:")
-        for team in available_teams:
-            eprint(f" - {team['display_name']}: {team['id']}")
-        return
-
-@app.route("/russianroulette")
+@app.route("/russianroulette", methods=["POST"])
 def russian_roulette():
-    data = request.get_json()
-    channel = data["channel_id"]
-    mm.add_user_to_channel(user["id"], channel)
+    # Make sure the bot is in the channel
+    channel = request.form["channel_id"]
+    mm.add_user_to_channel(channel, user["id"])
     return "PANG"
-
-if __name__ == "__main__":
-    main()
