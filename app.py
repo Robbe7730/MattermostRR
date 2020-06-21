4fe33811ed74247d7cf93b6b72871dc7e817ee03
Robbe Van Herck <robbe@robbevanherck.be>
2020-06-21 18:58:33 +0000

    Implement russian roulette (not really)

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import mattermost
-import mattermost.ws
 
 from flask import request
 from flask import Flask
@@ -9,6 +8,8 @@ app = Flask(__name__)
 
 import configparser
 import sys
+import time
+import random
 
 # Read in the config
 config = configparser.ConfigParser()
@@ -19,12 +20,40 @@ mm = mattermost.MMApi(config["mattermost"]["url"])
 mm.login(bearer=config["mattermost"]["token"])
 user = mm.get_user()
 
+# Setup russian roulette config
+active_users_since = int(config["russianroulette"]["active_users_since_minutes"])
+
 def eprint(msg):
     print(msg, file=sys.stderr)
 
 @app.route("/russianroulette", methods=["POST"])
 def russian_roulette():
-    # Make sure the bot is in the channel
-    channel = request.form["channel_id"]
-    mm.add_user_to_channel(channel, user["id"])
-    return "PANG"
+    try:
+        # Make sure the bot is in the channel
+        channel = request.form["channel_id"]
+        mm.add_user_to_channel(channel, user["id"])
+    except mattermost.ApiException:
+        return "I do not have permission to join this channel"
+
+    # Get all users that have posted recently
+    curr_millis = int(round(time.time() * 1000))
+    delay_millis = active_users_since * 60 * 1000
+    recent_posts = list(get_posts_for_channel(channel, curr_millis-delay_millis))
+    recent_users = set([x["user_id"] for x in recent_posts if x["user_id"] != user["id"]])
+
+    # Pick one
+    victim = mm.get_user(random.sample(recent_users, 1)[0])
+
+    # Notify the channel
+    mm.create_post(channel, f"BANG, goodbye @{victim['username']}")
+
+    # Kick them
+    mm.remove_user_from_channel(channel, victim["id"])
+    return "The gun was fired"
+
+# Based on the mattermost library, but that has no "since" argument
+def get_posts_for_channel(channel_id, since):
+    data_page = mm._get("/v4/channels/"+channel_id+"/posts", params={"since":str(since)})
+
+    for order in data_page["order"]:
+        yield data_page["posts"][order]
